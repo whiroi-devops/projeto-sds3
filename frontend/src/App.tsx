0c5ac3989fbbb9006425ae85edf50757c967f5cb
Willyan Hiroi <whiroi123@gmail.com>
2021-05-09 23:42:42 -0400

    Basic Static Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/datatable";
+
 function App() {
   return (
-    <div className="text-primary">
-      <h1>Olá Mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
